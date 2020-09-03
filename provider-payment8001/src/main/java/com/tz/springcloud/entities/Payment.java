181359e725f82f8b973a9f688fe7dd7f556b6c2a
TzashiNorpu <TzashiNorpu@gmail.com>
2020-09-03 18:19:23 +0800

    SpringCloudInit

---
+++ provider-payment8001/src/main/java/com/tz/springcloud/entities/Payment.java
@@ -0,0 +1,22 @@
+package com.tz.springcloud.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Payment implements Serializable {
+    private Long id;
+    private String serial;
+}
+
+/*
+
+create table `payment`(
+        `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT 'ID',
+        `serial` varchar(200) DEFAULT '',
+        PRIMARY KEY(`id`)
+        ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8*/
