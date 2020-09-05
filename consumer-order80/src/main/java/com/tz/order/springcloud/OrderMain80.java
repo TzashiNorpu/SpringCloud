dbcb2de3e88c65307dcb0b60b3eeacf1f14c3bd6
TzashiNorpu <yzsnuaa@126.com>
2020-09-05 14:11:43 +0800

    tututu

--- consumer-order80/src/main/java/com/tz/order/springcloud/OrderMain80.java
+++ consumer-order80/src/main/java/com/tz/order/springcloud/OrderMain80.java
@@ -3,16 +3,14 @@ package com.tz.order.springcloud;
 //import com.tz.order.myrule.MySelfRule;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 /*import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.cloud.netflix.ribbon.RibbonClients;*/
 
-/**
- * @auther zzyy
- * @create 2020-02-18 17:20
- */
+
 @SpringBootApplication
-//@EnableEurekaClient
+@EnableEurekaClient
 //@RibbonClient(name = "CLOUD-PAYMENT-SERVICE",configuration=MySelfRule.class)
 public class OrderMain80
 {
---
+++ provider-payment8002/src/main/java/com/tz/springcloud/service/impl/PaymentServiceImpl.java
@@ -0,0 +1,27 @@
+package com.tz.springcloud.service.impl;
+
+import com.tz.springcloud.entities.Payment;
+import com.tz.springcloud.dao.PaymentDao;
+import com.tz.springcloud.service.PaymentService;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+
+
+@Service
+public class PaymentServiceImpl implements PaymentService
+{
+    // JDK 的依赖注入
+    @Resource
+    private PaymentDao paymentDao;
+
+    public int create(Payment payment)
+    {
+        return paymentDao.create(payment);
+    }
+
+    public Payment getPaymentById(Long id)
+    {
+        return paymentDao.getPaymentById(id);
+    }
+}
